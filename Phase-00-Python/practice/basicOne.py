ee768326167edfcd1b170c0ea3b030f9003f7bd2
albenus murmu <albenusm@gmail.com>
2026-08-18 14:38:13 +0530

    Write Python code

---
+++ Phase-00-Python/practice/basicOne.py
@@ -0,0 +1,26 @@
+# name = input('Enter yr name ')
+# color = input("Enter your fav color ")
+# print(f'My name is {name} and my fav color is {color}')
+
+# (Day 2)
+# ##Calculate your age
+#
+# birth_year = input("Enter birth year: ")
+# print(type(birth_year))
+# age = 2026 - int(birth_year)
+# ##why int, cuz return string '2026' like that
+# print(age)
+
+# Ask a user their weight (in pounds), convert it to kilograms and print on the terminal
+
+user_weight_lbs = input("Enter your weight(lbs): ")
+
+user_weight_kgs = float(user_weight_lbs) * 0.45
+
+# print("In kG : ", user_weight_kgs)
+# print("In kG : ", user_weight_kgs)
+# or
+# print("In kG : " + str(user_weight_kgs))
+# or
+print(f'In kg {user_weight_kgs}')
+
