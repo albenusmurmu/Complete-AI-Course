08004e634dcbb29ef0306fd0602b89037ac07d89
albenus murmu <albenusm@gmail.com>
2026-09-07 13:46:48 +0530

    Implemented List

---
+++ Phase-00-Python/practice/list.py
@@ -0,0 +1,9 @@
+
+Student_Names = ["Peter", "Pareek", "Albenus", "Murmu"]
+for s_n in Student_Names:
+    print(s_n)
+first_name = Student_Names[1]
+print("First Name : "+first_name)
+first_name = "Albenus Murmu"
+print("First Name01 : "+first_name)
+print("Student_Name : "+ Student_Names[0]) # can't change in original list
