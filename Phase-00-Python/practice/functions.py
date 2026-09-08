b7fe9de763b57a642d1e7ec20cdcee5173e8de39
albenus murmu <albenusm@gmail.com>
2026-09-08 12:09:39 +0530

    Implemented Functions

--- Phase-00-Python/practice/functions.py
+++ Phase-00-Python/practice/functions.py
@@ -8,10 +8,12 @@ print("Hello World04")
 print("Hello World05")
 
 #02
-
+# here we pass the parameters
 def user_data(name,email,password):
     print(f'User_Name : {name}')
     print(f'User_Email : {email}')
     print(f'User_Pass : {password}')
-
+# here we pass the arguments
 user_data("Peter","peter@gmail.com","Peter@123#")
+
+#03
