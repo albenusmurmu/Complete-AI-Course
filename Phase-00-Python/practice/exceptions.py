af4e7a4ea2d8315369a54b7e41a8af70473087e4
albenus murmu <albenusm@gmail.com>
2026-09-09 14:47:36 +0530

    Practice eceptions with different logics

--- Phase-00-Python/practice/exceptions.py
+++ Phase-00-Python/practice/exceptions.py
@@ -1,7 +1,20 @@
 # exception trying to find the out error and resolve
 #  Study about the types of error and how to handle them like valueErrors, IndentationError etc.
+
+# 01
 try:
     age = int(input('Enter yr age : '))
     print(f'{age} years old')
 except ValueError:
     print('Please enter a valid age')
+    
+# 02
+try:
+    age = int(input('Enter yr age : '))
+    income = int(input('Enter yr income : '))
+    risk = income / age
+    print(f'{risk} you can take the risk')
+except ValueError:
+    print('Please enter a valid age')
+except ZeroDivisionError:
+    print('age must be 1 or greater then 1')
