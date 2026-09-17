265e05797f52d0abb395e6a782e3a1550ba657a4
albenus murmu <albenusm@gmail.com>
2026-09-17 14:56:55 +0530

    print random name

--- Phase-00-Python/practice/random_value.py
+++ Phase-00-Python/practice/random_value.py
@@ -6,3 +6,15 @@ for i in range(3):
     
 name = ["peter","parker","Andrew","Mosh"]
 print(random.choice(name))
+
+# task :- generate the random vlaues(2,4) like this
+
+class Dice:
+    def roll(self):
+        first = random.randint(1,6)
+        second = random.randint(1,6)
+        # return first,second # // always add tuple by default
+        return (first,second)
+    
+dice = Dice()
+print(dice.roll())
