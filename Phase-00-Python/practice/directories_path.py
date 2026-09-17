3c777c83572ea28ba15388aeef7592faf88415fd
albenus murmu <albenusm@gmail.com>
2026-09-17 15:24:20 +0530

    Implemented Directories

--- Phase-00-Python/practice/directories_path.py
+++ Phase-00-Python/practice/directories_path.py
@@ -4,3 +4,7 @@ path = Path("note")
 print(path.exists()) #// True cuz it exist
 print(path.mkdir()) #// return none cuz it create the new directories , can delete through this command "rmdir()"
 
+# search all the files 
+path = Path()
+for file in path.glob('*.py'):
+    print(file)
